8fe15671342138c6bc1fe1d60f3e46047861c2e9
Zachary Kearns <zach_kearns89@live.com>
2017-05-18 00:28:55 -0400

    finish heroku config

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routes import *
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/votingapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://psciupthjhnfbi:d18c73a8cc96fc3733507a5144a94c8fe4747ae77b0584541046d32b3d705e8c@ec2-184-73-236-170.compute-1.amazonaws.com:5432/dcj3a6sju72t1i'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
